81711497ec41df7f6221267b56e4970bc20643f5
frivolousvision <frivolousvision@gmail.com>
2021-10-02 20:45:49 -0400

    implemented sliding mobile nav menu

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,51 +1,108 @@
 import "./header.css";
-import React from "react";
-import { Link, NavLink } from "react-router-dom";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSun, faMoon, faBars } from "@fortawesome/free-solid-svg-icons";
+import {
+  faSun,
+  faMoon,
+  faBars,
+  faTimes,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Header = (props) => {
+  const [showNav, setShowNav] = useState(false);
+  const [displayContent, setDisplayContent] = useState(false);
+  const toggleMobileNav = () => {
+    if (showNav) {
+      setShowNav(false);
+      setDisplayContent(false);
+    }
+    if (!showNav) {
+      setShowNav(true);
+      setTimeout(() => {
+        setDisplayContent(true);
+      }, 200);
+    }
+  };
   return (
-    <nav className={`header ${props.dark ? "header-dark" : "header-light"}`}>
-      <ul
-        className={`header-content ${
-          props.dark ? "header-content-dark" : "header-content-light"
-        }`}
-      >
-        <FontAwesomeIcon
-          icon={faBars}
-          className={`bars ${props.dark ? "bars-dark" : "bars-light"}`}
-        />
-        <NavLink to='/'>
-          <li>about</li>
-        </NavLink>
-        <Link to='/projects'>
-          <li>projects</li>
-        </Link>
-        <Link to='/contact'>
-          <li>contact</li>
-        </Link>
-        <div
-          className={`toggle-container ${
-            props.dark ? "toggle-container-dark" : "toggle-container-light"
+    <div>
+      <nav className={`header ${props.dark ? "header-dark" : "header-light"}`}>
+        <ul
+          className={`header-content ${
+            props.dark ? "header-content-dark" : "header-content-light"
           }`}
         >
-          {props.dark ? (
+          <FontAwesomeIcon
+            icon={faBars}
+            className={`bars ${props.dark ? "bars-dark" : "bars-light"}`}
+            onClick={toggleMobileNav}
+          />
+          <Link to='/'>
+            <li>about</li>
+          </Link>
+          <Link to='/projects'>
+            <li>projects</li>
+          </Link>
+          <Link to='/contact'>
+            <li>contact</li>
+          </Link>
+          <div
+            className={`toggle-container ${
+              props.dark ? "toggle-container-dark" : "toggle-container-light"
+            }`}
+          >
+            {props.dark ? (
+              <FontAwesomeIcon
+                onClick={props.toggleDark}
+                icon={faMoon}
+                className='moon'
+              />
+            ) : (
+              <FontAwesomeIcon
+                onClick={props.toggleDark}
+                icon={faSun}
+                className='sun'
+              />
+            )}
+          </div>
+        </ul>
+      </nav>
+      <nav
+        className={`mobile-nav ${
+          props.dark ? "mobile-nav-dark" : "mobile-nav-light"
+        }
+           ${showNav ? "mobile-nav-show" : "mobile-nav-hide"}`}
+      >
+        <ul
+          className={`mobile-nav-content ${
+            props.dark ? "mobile-nav-content-dark" : "mobile-nav-content-light"
+          } ${
+            displayContent
+              ? "mobile-nav-content-show"
+              : "mobile-nav-content-hide"
+          }`}
+        >
+          <li>
             <FontAwesomeIcon
-              onClick={props.toggleDark}
-              icon={faMoon}
-              className='moon'
+              icon={faTimes}
+              onClick={toggleMobileNav}
+              className={`fa-times ${
+                props.dark ? `fa-times-dark` : `fa-time-light`
+              }`}
             />
-          ) : (
-            <FontAwesomeIcon
-              onClick={props.toggleDark}
-              icon={faSun}
-              className='sun'
-            />
-          )}
-        </div>
-      </ul>
-    </nav>
+          </li>
+          <Link to='/'>
+            <li onClick={toggleMobileNav}>about</li>
+          </Link>
+          <Link to='/projects'>
+            <li onClick={toggleMobileNav}>projects</li>
+          </Link>
+          <Link to='/contact'>
+            <li onClick={toggleMobileNav}>contact</li>
+          </Link>
+        </ul>
+      </nav>
+    </div>
   );
 };
 export default Header;
