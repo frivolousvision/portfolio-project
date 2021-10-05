5994cd8e0ee23071e057b04503a86680728ee9fe
frivolousvision <frivolousvision@gmail.com>
2021-10-04 23:51:31 -0400

    silly menu notification

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,7 +1,18 @@
 import "./home.css";
+import { useState, useEffect } from "react";
 import DanPhoto from "./Dan-4.21.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShare } from "@fortawesome/free-solid-svg-icons";
 
 const Home = (props) => {
+  const [menuIcon, setMenuIcon] = useState(false);
+  useEffect(() => {
+    setTimeout(() => {
+      if (!menuIcon) setMenuIcon(true);
+      if (menuIcon) setMenuIcon(false);
+    }, 3000);
+  });
+
   return (
     <div>
       <div
@@ -10,6 +21,9 @@ const Home = (props) => {
           props.showNav ? "show-nav" : "hide-nav"
         }`}
       >
+        <p className={menuIcon ? "menu-icon-show" : "menu-icon-hide"}>
+          <FontAwesomeIcon icon={faShare} className='fa-share' /> menu!
+        </p>
         <div
           className={`home-container ${
             props.dark ? "home-container-dark" : "home-container-light"
