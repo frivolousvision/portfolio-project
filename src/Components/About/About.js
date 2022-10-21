46af83798c182f88aa5a9e90accaef18d84a8277
frivolousvision <frivolousvision@gmail.com>
2022-10-21 16:04:19 -0400

    Updated about section

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -15,6 +15,7 @@ const About = (props) => {
       }`}
     >
       <div
+        onClick={props.closeAllNav}
         className={`about-container ${
           dark ? "about-container-dark" : "about-container-light"
         }`}
@@ -27,52 +28,58 @@ const About = (props) => {
         {/* <div className='resume-container'>
           <img src={resumeLight} />
         </div> */}
-        <div className='about-text-container'>
+        <div className='about-text-container' onClick={props.closeAllNav}>
           <h2 className='about-header'>About</h2>
           <p>
-            I'm an Atlanta based developer specializing in the PERN stack who
-            loves learning, problem-solving, creating dynamic content, and
-            working with people. A strong foundation in modern web development
-            languages such as JavaScript ES6, CSS3, and HTML5, with an affinity
-            for creating dynamic single-page applications with React and Redux.
-            Proficient in building responsive UI's that delight users with their
-            functionality and a sharp ability to design pages based on
-            wireframes. Proficient in using NodeJS to build the backend under
-            the Express framework to create servers and route requests to manage
-            CRUD functionality. Experience in relational databases creating and
-            working with RESTful APIs to manage and deliver content via
-            PostgreSQL. Extensive experience in customer service building and
-            managing relationships.
+            I'm an Atlanta based Frontend / Fullstack developer excelling in
+            learning, problem-solving, and architecting modern applications.
+            Highly motivated self-starter who loves working with people.
+            Knowledge in current web development technologies, with an affinity
+            for creating dynamic single-page applications. Experience building
+            responsive UI's with clean functionality with precise attention to
+            design details based on wire frames. Skilled in using backend
+            frameworks to create services to deliver and manage data through
+            relational databases. Extensive experience in customer service -
+            building and maintaining client relationships.
           </p>
           <h2 className='skills-header'>Technologies I Use</h2>
           <div className='skills-container'>
             <div>
               <ul>
-                <h3>Front:</h3>
+                <h3>Frontend:</h3>
+                <li>TypeScript</li>
                 <li>JavaScript ES6</li>
-                <li>CSS3</li>
                 <li>React</li>
+                <li>JSX / TSX</li>
                 <li>Redux</li>
+                <li>CSS3</li>
+                <li>Ant Design</li>
+                <li>Figma</li>
                 <li>HTML5</li>
-                <li>Responsive UI's</li>
-                <li>Adobe Illustrator</li>
               </ul>
             </div>
             <div>
               <ul>
-                <h3>Back:</h3>
-                <li>NodeJS</li>
-                <li>Express</li>
+                <h3>Backend:</h3>
+                <li>Node.js</li>
+                <li>Express.js</li>
                 <li>PostgreSQL</li>
-                <li>RESTful APIs</li>
+                <li>SQL Server</li>
+                <li>C# / ASP.NET</li>
+                <li>Database Design</li>
               </ul>
             </div>
             <div>
               <ul>
-                <h3>Bit of Both:</h3>
+                <h3>Other Competancies:</h3>
+                <li>RESTful APIs</li>
+                <li>Responsive Design</li>
+                <li>AJAX</li>
                 <li>Git/Github</li>
+                <li>Azure</li>
+                <li>Netlify</li>
                 <li>Heroku</li>
-                <li>Continuous Integration</li>
+                <li>CI / CD</li>
               </ul>
             </div>
           </div>
