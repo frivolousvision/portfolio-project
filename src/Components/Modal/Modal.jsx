b7f11119e976b68b7c3e925a7b77fd7661e02961
frivolousvision <frivolousvision@gmail.com>
2023-12-16 17:47:47 -0500

    click outside modal

--- src/Components/Modal/Modal.jsx
+++ src/Components/Modal/Modal.jsx
@@ -1,8 +1,23 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
 import "./modal.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Modal = ({ skill, skillType, index, handleEditSkill, closeModal }) => {
   const [value, setValue] = useState(skill);
+  const ref = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (ref.current && !ref.current.contains(event.target)) {
+        closeModal();
+      }
+    };
+    document.addEventListener("click", handleClickOutside, true);
+    return () => {
+      document.removeEventListener("click", handleClickOutside, true);
+    };
+  }, [closeModal]);
 
   const handleKeyPress = (event) => {
     if (event.key === "Enter") {
@@ -11,7 +26,12 @@ const Modal = ({ skill, skillType, index, handleEditSkill, closeModal }) => {
   };
 
   return (
-    <div className='edit-modal-container'>
+    <div ref={ref} className='edit-modal-container'>
+      <FontAwesomeIcon
+        onClick={closeModal}
+        icon={faTimes}
+        className={`close-edit-modal`}
+      />
       <input
         autoFocus
         type='text'
