e37df3d5534cae129f8e3b27450cfaf82faad111
frivolousvision <frivolousvision@gmail.com>
2024-06-27 13:48:35 -0400

    Content and styling updates

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -4,21 +4,7 @@ import DanPhoto from "./dan-photo-3.jpg";
 import { DarkContext } from "../../App";
 
 const Home = (props) => {
-  // const [menuIcon, setMenuIcon] = useState(false);
-  // const [firstVisit, setFirstVisit] = useState(true);
   const dark = useContext(DarkContext);
-
-  // useEffect(() => {
-  //   setTimeout(() => {
-  //     setMenuIcon(true);
-  //   }, 2000);
-  //   setTimeout(() => {
-  //     setMenuIcon(false);
-  //   }, 6000);
-  // }, []);
-  // setTimeout(() => {
-  //   setFirstVisit(false);
-  // }, 1000 * 10);
 
   return (
     <div
@@ -27,63 +13,40 @@ const Home = (props) => {
         props.showNav ? "show-nav" : "hide-nav"
       }`}
     >
-      {/* <p
-        className={`menu-icon ${
-          menuIcon ? "menu-icon-show" : "menu-icon-hide"
-        }`}
-      >
-        <FontAwesomeIcon icon={faShare} className='fa-share' /> menu!
-      </p> */}
       <div
         onClick={props.closeAllNav}
         className={`home-container ${
           dark ? "home-container-dark" : "home-container-light"
         }`}
       >
-        <div className='image-container'>
-          <img src={DanPhoto} alt='' />
+        <div className="image-container">
+          <img src={DanPhoto} alt="" />
         </div>
-        <div className='text-container'>
-          <h2
-            className='home-header '
-            //   ${
-            //    firstVisit ? "home-header-show" : "home-header-hide"}`
-            // }
-          >
-            Hey there! My name's Dan and I make web applications
+        <div className="text-container">
+          <h2 className="home-header ">
+            Hi there! My name's Dan and I make web applications
           </h2>
-
-          {/* <h2
-            className={`goodbye-header ${
-              firstVisit ? "goodbye-header-hide" : "goodbye-header-show"
-            }`}
-          >
-            Thanks for stopping by! Hope to see you around!
-          </h2> */}
           <div className={`home-about-text`}>
             <p>
-              I'm an Atlanta based Frontend / Fullstack developer. I love
-              learning, solving problems, creating dynamic apps, and working
-              with people.
+              I'm an Atlanta based Full Stack developer. I love learning,
+              solving problems, creating dynamic apps, and working with people.
             </p>
             <br />
             <br />
             <p>
               I have experience using modern web development technologies such
-              as TypeScript, JavaScript ES6, CSS3, HTML5, and like creating
-              dynamic single-page applications with React and Redux. I am
-              proficient in building screen responsive, user-friendly UI's and
-              have a sharp ability to design pages based on Figma wireframes.
+              as React, Svelte, Vue, TypeScript, JavaScript ES6, CSS3, HTML5.
+              I'm proficient in building responsive, user-friendly UI's and have
+              a sharp ability to design pages based on Figma wireframes.
             </p>
             <br />
             <br />
             <p>
-              I am well versed in backend services using NodeJS / Express and C#
-              / ASP.NET to create servers and route requests to implement CRUD
-              functionality. I have experience working with relational databases
-              and RESTful APIs, connect the client and service to deliver
-              dynamic content. Also, I have very extensive experience in
-              customer service building and maintaining relationships.
+              I am well versed in backend services using languages such as Node,
+              Python and Java. I have experience working with relational
+              databases and RESTful APIs, to deliver dynamic data-driven
+              content. Also, I have very extensive experience in hospitality
+              building and maintaining relationships.
             </p>
           </div>
         </div>
